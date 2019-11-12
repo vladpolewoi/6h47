3587ed01fbe84ca63ef4831f94ec929bf6915e12
vlad polewoi <vlad.polewoi1@gmail.com>
2019-11-12 14:18:25 -0800

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+div
+  router-link(to="/home") home
+  router-link(to="/chat") chat
+  router-view
+</template>
+
+<style lang="scss">
+@import './../node_modules/bulma/css/bulma.css';
+</style>
